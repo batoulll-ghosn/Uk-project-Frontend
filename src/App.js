6548385f688998567accf727376f3747f4682190
unknown <batoul.ghosn@isae.edu.lb>
2023-12-31 01:00:11 +0200

    middle section need fetching

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import Navbar from './components/navbar';
 import Header from './components/header';
 import Services from './components/services';
+import MiddleSection from './components/MiddleSection'
 function App() {
   return (
     <>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins&display=swap" />
-    <Navbar/>
-    <Header />
-    <Services />
+    
+    <MiddleSection />
     </>
   );
 }
