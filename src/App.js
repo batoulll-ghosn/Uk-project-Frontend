5f7e96381fa4ffcfae714766774d20384b0b8c26
unknown <batoul.ghosn@isae.edu.lb>
2024-01-01 22:15:35 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/navbar';
 import Header from './components/header';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Services from './components/services';
 import MiddleSection from './components/MiddleSection';
 import Slice from './components/slice';
@@ -8,20 +8,16 @@ import Login from './components/login';
 import Test from './components/test';
 function App() {
   return (
-    <>  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins&display=swap" />
   <Router>
+  <div className="App">
    <Routes>
+        <Route path="/" element={<><Navbar /><Header /> <Slice /><Services /></>}/>
        <Route path="/login" element={ <Login />}/>
        <Route path="/batoul" element={ <Test />}/>
-       <Route path="/" element={<>
-    <Navbar />
-    <Header /> 
-    <Slice />
-    <Services />
-    </>}/>
+       
   </Routes>
+  </div>
   </Router>
-  </>
   );
 }
 
